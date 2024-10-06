d1c50e2fb959f93a55f1f8bf43df799134684a0b
Hashara13 <hnethmin13@gmail.com>
2024-10-06 16:07:13 +0530

    minor changes

--- urls/routes.py
+++ urls/routes.py
@@ -10,5 +10,27 @@ def setUrl(length=6):
 @app.route('/', method=['GET', 'POST'])
 def index():
     if request.method=="POST":
+        longUrl=request.form(longUrl)
+        shortUrl=setUrl()
+        newUrl=URL(longUrl=longUrl,shortUrl=shortUrl)
+        db.session.add(newUrl)
+        db.session.commit()
+        
+        return jsonify({'shortUrl':request.host_url+shortUrl})
+    return render_template('index.html')
+
+@app.route('<shortUrl>')
+def returnUrl(shortUrl):
+    url=URL.query.filter_by(shortUrl=shortUrl).first_or_404()
+    url.clickCount=1
+    db.session.commit()
+    return redirect(url.longUrl)
+
+@app.route('count/<shortUrl>')
+def countClick(shortUrl):
+    url=URL.query.filter_by(shortUrl=shortUrl).first_or_404()
+    return jsonify({'shortUrl': url.shortUrl, 'clickCount': url.clickCount})
+
+    
         
     
