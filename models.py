7c8eb642bbd707bf2f363785519a2bf562bb2abc
Hashara13 <hnethmin13@gmail.com>
2024-10-06 15:52:06 +0530

    model amended

--- models.py
+++ models.py
@@ -3,4 +3,9 @@ from app import db
 class URL(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     longUrl=db.Column(db.String(2000),nullable=False)
+    shortUrl=db.Column(db.String(15),unique=True,nullable=False)
+    clickCount=db.Column(db.Integer,default=0)
+    
+    def __repr__(self):
+        return f"<URL {self:longUrl} -> {self:shortUrl}>"
     
